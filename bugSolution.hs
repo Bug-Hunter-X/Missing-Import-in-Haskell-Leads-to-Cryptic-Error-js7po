3d5c1b27068ee1871c1bfa7cb4c48d979d08e9d6
BugHunter-X <askzenai@gmail.com>
2025-01-21 10:16:04 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,9 @@
+The solution is simple: import the required module, `Data.List`.
+
+```haskell
+import Data.List
+
+main :: IO ()
+main = print $ length [1..10]
+```
+Now the code compiles and runs correctly.
